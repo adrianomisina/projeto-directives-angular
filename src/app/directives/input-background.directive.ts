020463db5920134a74e2365eb1e6c01e5687f941
adrianomisina <adriano.misina@gmail.com>
2024-04-09 12:33:56 -0300

    Como Passar Parâmetros para uma Diretiva de Atributo

--- src/app/directives/input-background.directive.ts
+++ src/app/directives/input-background.directive.ts
@@ -5,12 +5,21 @@ import { Directive, HostBinding, HostListener, Input } from "@angular/core";
 })
 
 export class InputBackgroundDirective {
-  @Input() appInputBackground: string = 'white';
+  @Input('bgColor') appInputBackground: string = 'white';
+  @Input('tColor') textColor: string = 'black';
 
   @HostBinding('style.backgroundColor') bgColor: string = ''
+  @HostBinding('style.color') color: string = 'white  ';
 
   @HostListener('focus') 
   onFocus() {
     this.bgColor = this.appInputBackground;
+    this.color = this.textColor
+  }
+
+  @HostListener('blur')
+  onBlur() {
+    this.bgColor = 'white';
+    this.color = 'black';
   }
 }
